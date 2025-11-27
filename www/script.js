7740aebcfa5bf9962971cc6bf4cd7a236e1e4388
arisudan <pradhanarisudan@gmail.com>
2025-11-27 23:19:04 +0530

    slidebar added

--- www/script.js
+++ www/script.js
@@ -1,3 +1,11 @@
-document.querySelector(".cart-icon").addEventListener("click", () => {
-    console.log("Cart clicked — slide panel coming soon!");
+const cartIcon = document.querySelector(".cart-icon");
+const cartPanel = document.getElementById("cart-panel");
+const closeCart = document.querySelector(".close-cart");
+
+cartIcon.addEventListener("click", () => {
+    cartPanel.classList.add("active");
+});
+
+closeCart.addEventListener("click", () => {
+    cartPanel.classList.remove("active");
 });
